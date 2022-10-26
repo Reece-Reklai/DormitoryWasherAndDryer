fe79e96b2966dcf97805bea2d5865f91d4148e89
thomca <catherine.thomsen@wallawalla.edu>
2022-10-25 22:30:53 -0700

    I need to go to bed. Made some edits to drop down class to make it dynamic and still need to hash out selection method. Also dragged out class details which need to be made into the right structure. Generally more things to do

--- wwu_wash_and_dry/lib/home_widgets.dart/drop_down_list.dart
+++ wwu_wash_and_dry/lib/home_widgets.dart/drop_down_list.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DropDownList extends StatefulWidget {
-  const DropDownList({super.key});
+  final List<String> floorList;
+  const DropDownList(this.floorList, {super.key});
 
   @override
   State<DropDownList> createState() => _DropDownListState();
@@ -9,33 +10,30 @@ class DropDownList extends StatefulWidget {
 
 // dropDown menu example
 // https://flutterforyou.com/how-to-add-dropdown-menu-in-flutter/
+// https://www.flutterbeads.com/dropdown-in-flutter/
 class _DropDownListState extends State<DropDownList> {
   int selectedValue = 1;
-
   @override
   Widget build(BuildContext context) {
+    String dropdownValue = widget.floorList[selectedValue];
     return Center(
-      child: DropdownButton(
-          value: selectedValue,
-          items: const [
-            DropdownMenuItem(
-              value: 1,
-              child: Text("F1"),
+      child: DropdownButton<String>(
+        value: dropdownValue,
+        items: widget.floorList.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(
+              value,
+              style: const TextStyle(fontSize: 30),
             ),
-            DropdownMenuItem(
-              value: 2,
-              child: Text("F2"),
-            ),
-            DropdownMenuItem(
-              value: 3,
-              child: Text("F3"),
-            )
-          ],
-          onChanged: (value) {
-            setState(() {
-              selectedValue = value as int;
-            });
-          }),
+          );
+        }).toList(),
+        onChanged: (newValue) {
+          setState(() {
+            dropdownValue = newValue as String;
+          });
+        },
+      ),
     );
   }
 }
