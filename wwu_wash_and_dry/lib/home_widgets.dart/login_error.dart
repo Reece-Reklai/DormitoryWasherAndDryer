a9fcd5addd126b2908e1f11e939089026bcbe87a
Adam Taylor <adamt@wavecable.com>
2022-10-27 11:20:41 -0700

    added comments and const to appropriate things

--- wwu_wash_and_dry/lib/home_widgets.dart/login_error.dart
+++ wwu_wash_and_dry/lib/home_widgets.dart/login_error.dart
@@ -13,21 +13,25 @@ class LoginError extends StatelessWidget {
           backgroundColor: Colors.lightGreen.shade900
         ),
         body: Column(
+          // allign the button and text to the center of the column
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // allign the button and text to the center of the screen
             Center(
               child: Container(
                       margin: const EdgeInsets.all(15.0),
                       child: const Text("Login Error",
-                      style: TextStyle(fontSize: 30),)
+                      style: const TextStyle(fontSize: 30),)
                     )
             ),
             Center(
               child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                        fixedSize: Size(150,50),
+                        fixedSize: const Size(150,50),
                         primary: Colors.lightGreen.shade900
                       ),
+                      // on button press update the state back to the
+                      // login state
                       onPressed: updateState,
                       child: const Text('Return to login'),
                     ) 
