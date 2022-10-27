1f0cbc0e0deb30eba0c4ffc78e8dc2ed92641b85
Adam Taylor <adamt@wavecable.com>
2022-10-27 10:49:31 -0700

    updated error page

--- wwu_wash_and_dry/lib/home_widgets.dart/login_error.dart
+++ wwu_wash_and_dry/lib/home_widgets.dart/login_error.dart
@@ -10,12 +10,30 @@ class LoginError extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('WWU Wash and Dry'),
+          backgroundColor: Colors.lightGreen.shade900
         ),
-        body: Column(children: [
-          const Text("Login Error"),
-          ElevatedButton(
-              onPressed: updateState, child: const Text('Return to login')),
-        ]),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                      margin: const EdgeInsets.all(15.0),
+                      child: const Text("Login Error",
+                      style: TextStyle(fontSize: 30),)
+                    )
+            ),
+            Center(
+              child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(150,50),
+                        primary: Colors.lightGreen.shade900
+                      ),
+                      onPressed: updateState,
+                      child: const Text('Return to login'),
+                    ) 
+            ),
+          ]
+        ),
       ),
     );
   }
