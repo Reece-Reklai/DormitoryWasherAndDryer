75f1c39bf4065d1093622a441c261828f1f12ec4
Bradon Ladd <bradon.laddy0614@gmail.com>
2022-11-02 21:35:25 -0700

    Integrated floorLayout, machine class added, home page restructured, etc

--- wwu_wash_and_dry/lib/home_widgets.dart/home_page.dart
+++ wwu_wash_and_dry/lib/home_widgets.dart/home_page.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 
 import './drop_down_list.dart';
+import './dorm_data.dart';
+import './machine.dart';
 import '../floor_layout.dart';
 
 // Implemented by Catherine Thomsen
@@ -14,6 +16,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool floorChoosen = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: DropDownMain());
+  }
+}
+
+class DropDownMain extends StatefulWidget {
+  const DropDownMain({super.key});
+
+  @override
+  State<DropDownMain> createState() => _DropDownMain();
+}
+
+class _DropDownMain extends State<DropDownMain> {
   static const _foremanFloors = ['F2', 'F3', 'F4', 'F5', 'F6', 'F7'];
   static const _condardFloors = ['C1', 'C2', 'C3', 'C4'];
   static const String _forman = "Foreman";
@@ -22,61 +40,46 @@ class _HomePageState extends State<HomePage> {
   String floor = 'none';
   String building = 'none';
   bool floorChoosen = false;
+
+  // Needs to pass in washers and dyers
   void updatePage(String selectedFloor, String selectedBuilding) {
-    floor = selectedFloor;
-    building = selectedBuilding;
-    setState(() {
-      floorChoosen = true;
-    });
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FloorLayout(machines: Sittner)),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!floorChoosen) {
-      return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('WWU Wash and Dry'),
-          ),
-          body: Column(
-            children: <Widget>[
-              const Text(
-                "Home Page",
-                style: TextStyle(fontSize: 30),
-              ),
-              TextButton(
-                onPressed: () => {
-                  building = _sittner,
-                  setState(() {
-                    floorChoosen = true;
-                  })
-                },
-                child: const Text(
-                  _sittner,
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                  ),
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('WWU Wash and Dry'),
+        ),
+        body: Column(
+          children: <Widget>[
+            const Text(
+              "Home Page",
+              style: TextStyle(fontSize: 30),
+            ),
+            TextButton(
+              onPressed: () => {
+                building = _sittner,
+                setState(() {
+                  updatePage("S1", _sittner);
+                })
+              },
+              child: const Text(
+                _sittner,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
                 ),
               ),
-              DropDownList(_forman, _foremanFloors, updatePage),
-              DropDownList(_conard, _condardFloors, updatePage),
-            ],
-          ),
+            ),
+            DropDownList(_forman, _foremanFloors, updatePage),
+            DropDownList(_conard, _condardFloors, updatePage),
+          ],
         ),
       );
-    } else {
-      if (building == _forman) {
-        return Container();
-        // return FloorLayout(floor);
-      } else if (building == _conard) {
-        return Container();
-        // return FloorLayout(floor);
-
-      } else {
-        return Container();
-        // return FloorLayout("S1");
-      }
-    }
   }
 }
---
+++ wwu_wash_and_dry/lib/home_widgets.dart/machine.dart
@@ -0,0 +1,21 @@
+// Created by Catherine Thomsen and Bradon Ladd
+
+class Machine {
+  Machine(this._id, this._type, this._floor);
+
+  final String _id;
+  final String _floor;
+  
+  final String _type;
+  String _status = '';
+
+  get id => _id;
+  get type => _type;
+  get floor => _floor;
+  get status => _status;
+
+  // MOCK
+  String getStatus() {
+    return "BROKEN";
+  }
+}
