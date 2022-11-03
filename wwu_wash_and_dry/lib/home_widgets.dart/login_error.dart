56906508f55d159d87f4a10666eba98ce35928a4
Adam Taylor <adamt@wavecable.com>
2022-11-02 19:52:09 -0700

    update private variables and added two tests

--- wwu_wash_and_dry/lib/home_widgets.dart/login_error.dart
+++ wwu_wash_and_dry/lib/home_widgets.dart/login_error.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LoginError extends StatelessWidget {
-  final VoidCallback updateState;
-  const LoginError(this.updateState, {super.key});
+  final VoidCallback _updateState;
+  const LoginError(this._updateState, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,28 +13,25 @@ class LoginError extends StatelessWidget {
           backgroundColor: Colors.lightGreen.shade900,
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Uh oh! There seems to have been an error",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-            
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Uh oh! There seems to have been an error",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
             const SizedBox(height: 25),
-
-              ElevatedButton(
-                onPressed: updateState,
-                style: TextButton.styleFrom(
-                  fixedSize: const Size.fromHeight(40.0),
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.lightGreen.shade900,
-                ),
-                child: const Text('Return to login page'),
-              )
-            ],
-          )
-        ),
+            ElevatedButton(
+              onPressed: _updateState,
+              style: TextButton.styleFrom(
+                fixedSize: const Size.fromHeight(40.0),
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.lightGreen.shade900,
+              ),
+              child: const Text('Return to login page'),
+            )
+          ],
+        )),
       ),
     );
   }
